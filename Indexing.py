590b407ff2870373d26baa60bb2d4a52b4173a2b
Ushravan <39063876+Ushravan@users.noreply.github.com>
2021-06-19 15:46:44 +0530

    Add files via upload

---
+++ Indexing.py
@@ -0,0 +1,8 @@
+# Indexing
+# Given a word W and an integer N, write a program to print the character present at the index N in the word W.
+
+W = input()
+N = input()
+N = int(N)
+part = W[N]
+print(part)
---
+++ Sum of two numbers.py
@@ -0,0 +1,9 @@
+# Sum of two numbers
+# Write a program to print the sum of two integer inputs A and B
+
+A = input()
+B = input()
+A = int(A)
+B = int(B)
+sum = A + B
+print(sum)
