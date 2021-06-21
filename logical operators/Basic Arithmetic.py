8c871dde5c9b90b0753fdfe2bcb368d0290f9677
Ushravan <39063876+Ushravan@users.noreply.github.com>
2021-06-21 15:53:58 +0530

    Create Basic Arithmetic.py

---
+++ logical operators/Basic Arithmetic.py
@@ -0,0 +1,24 @@
+#Basic Arithmetic
+#Write a program to take two integer inputs (say A and B) and print the result of the following operations:
+#1. Addition
+#2. Subtraction 
+#3. Multiplication
+
+#Output
+
+#The first line should contain the sum of the two given integers.
+#The second line should contain the subtraction of the two given integers.
+#The third line should contain the product of the two given integers.
+
+#Explanation
+
+#In the given example, the inputs are A = 4 and B = 3
+#Addition: 4 + 3 = 7
+#Subtraction: 4 - 3 = 1
+#Multiplication: 4 * 3 =  12
+
+A = int (input())
+B = int(input())
+print(A+B)
+print(A-B)
+print(A*B)
