43c1a0ab2810817192f234a606d3c3b274c791cb
Ushravan <39063876+Ushravan@users.noreply.github.com>
2021-06-21 16:02:54 +0530

    Create Kilometers to Meters.py

---
+++ logical operators/Kilometers to Meters.py
@@ -0,0 +1,29 @@
+#Kilometers to Meters
+#Write a program to take the number of kilometers as input and convert into meters and print the number of meters.
+#Note: 1 Kilometer = 1000 Meters
+#Input
+
+#The first line of input contains the number of kilometers.
+#The input will have a decimal part.
+#Output
+
+#The first line of output contains the number of meters.
+#The output should be an integer.
+#Explanation
+
+#For example, 1.2 kilometers is equals to 1200 meters
+
+A = float(input())
+K = int(A*1000)
+print(K)
+
+
+
+
+
+
+
+
+
+
+
