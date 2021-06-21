c8689dfd3bd7ff060362d41c299a2bec1ad31074
Ushravan <39063876+Ushravan@users.noreply.github.com>
2021-06-21 15:58:02 +0530

    Create Area of a Rectangle.py

---
+++ logical operators/Area of a Rectangle.py
@@ -0,0 +1,29 @@
+#Area of a Rectangle
+#Write a program to calculate the area of the Rectangle.
+#Note: Area of Rectangle = Length of Rectangle x Breadth of Rectangle
+#Input
+
+#The first line of input contains the length of the rectangle, which is an integer.
+#The second line of input contains the breadth of the rectangle, which is an integer.
+#Output
+
+#Print the Area of Rectangle
+
+
+#Explanation
+
+#For example, when the length is 4 and breadth is 3, then A = 3 × 4, therefore A = 12
+
+A = int(input())
+B = int(input())
+area_of_rectangle = A*B
+print(area_of_rectangle)
+
+
+
+
+
+
+
+
+
