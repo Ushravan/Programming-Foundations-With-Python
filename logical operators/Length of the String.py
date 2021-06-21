572c6d1b15608e12727b3d23bd06aa0e86c116ec
Ushravan <39063876+Ushravan@users.noreply.github.com>
2021-06-21 16:05:11 +0530

    Create Length of the String.py

---
+++ logical operators/Length of the String.py
@@ -0,0 +1,14 @@
+#Length of the String
+#Write a program to read a single line of input and print the number of characters in the input.
+#Note: Spaces are also characters.
+#Explanation
+
+#For example, if the given string is "Software" the number of characters are 8.
+
+A = str(input())
+B = len(A)
+print(B)
+
+
+
+
