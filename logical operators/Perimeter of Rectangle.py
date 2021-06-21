3cc4e11d19b2d0f6062f34f6c942ec4bb3bd420f
Ushravan <39063876+Ushravan@users.noreply.github.com>
2021-06-21 16:00:38 +0530

    Create Perimeter of Rectangle.py

---
+++ logical operators/Perimeter of Rectangle.py
@@ -0,0 +1,23 @@
+#Perimeter of Rectangle
+#Write a program to calculate the perimeter of a rectangle.
+#Note: Perimeter of Rectangle = 2(Length of Rectangle + Breadth of Rectangle)
+#Input
+
+#The first line of input contains the length of the rectangle, which is an integer.
+#The second line of input contains the breadth of the rectangle, which is an integer.
+#Output
+
+#Print the Perimeter of the Rectangle
+#Explanation
+
+#For example, when the length of the rectangle is 3 and breadth is 4, then
+#Perimeter = 2 x ( 3 + 4 ) = 2 x 7 = 14
+
+A = int(input())
+B = int (input())
+perimeter_of_rectangle = 2*(A+B)
+print(perimeter_of_rectangle)
+
+
+
+
