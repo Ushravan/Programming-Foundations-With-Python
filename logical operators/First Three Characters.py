c165b3a9b7bdd01312da62a56173e625a0101800
Ushravan <39063876+Ushravan@users.noreply.github.com>
2021-06-21 16:06:40 +0530

    Create First Three Characters.py

---
+++ logical operators/First Three Characters.py
@@ -0,0 +1,14 @@
+#First Three Characters
+#Write a program to read a single line of input and print the first three characters in it.
+#Explanation
+
+#For example, if the given input is "Four" the first three characters are "Fou"
+#Similarly, the first three characters of "Strawberry" are "Str"
+
+A = str(input())
+B = A[0:3]
+print(B)
+
+
+
+
