2d2a6ca1ab7c2ad8c3c9f8ed1c60a4525a75ef64
Ushravan <39063876+Ushravan@users.noreply.github.com>
2021-06-25 14:42:09 +0530

    Create String _Repetition_2.py

---
+++ Conditional Statements/String _Repetition_2.py
@@ -0,0 +1,19 @@
+#String Repetition 2
+#Given a word and a number N, write a program to print the given word, N number of times in a single line.
+
+#Explanation
+
+#For example, if the given word is "Maths", and the N is 2,the word has to be repeated 2 times, so the output should be
+#"MathsMaths"
+
+Word = str(input())
+N = int(input())
+Repeate = (Word*N)
+
+print(Repeate)
+
+
+
+
+
+
