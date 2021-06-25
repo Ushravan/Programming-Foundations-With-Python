f68c3b87c54ec49fb233f71851237123155d22c6
Ushravan <39063876+Ushravan@users.noreply.github.com>
2021-06-25 16:51:11 +0530

    Create Greatest Among Two Numbers.py

---
+++ Operators&Conditional_Statements/Greatest Among Two Numbers.py
@@ -0,0 +1,15 @@
+#Greatest Among Two Numbers
+#Write a program to print the greatest among the two numbers.
+
+#Explanation
+
+#For example, if the given numbers are 4 and 3, your code should print the greatest number, which is 4.
+
+A = int(input())
+B = int(input())
+compare = (A > B) 
+if compare:
+    print(A)
+else:
+    print(B)
+    
