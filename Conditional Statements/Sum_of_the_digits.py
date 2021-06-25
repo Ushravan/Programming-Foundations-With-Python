217cfcc7f67cea2e8af04ba5e6ff35a283e957fd
Ushravan <39063876+Ushravan@users.noreply.github.com>
2021-06-25 14:45:23 +0530

    Create Sum_of_the_digits.py

---
+++ Conditional Statements/Sum_of_the_digits.py
@@ -0,0 +1,14 @@
+#Sum of the digits
+#Write a program that prints the sum of the digits of a given three-digit number.
+
+#Explanation
+
+#For example, if the given number is 326, the sum of its digits, 3 + 2 + 6 is 11.
+
+A = str(input())
+K = A[0]+A[1]+A[2]
+print(K)
+
+
+
+
