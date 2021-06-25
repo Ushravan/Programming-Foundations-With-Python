a8d03f2d2971fe4377219f4b952de31e225439c1
Ushravan <39063876+Ushravan@users.noreply.github.com>
2021-06-25 14:38:06 +0530

    Create Last_Character.py

---
+++ Conditional Statements/Last_Character.py
@@ -0,0 +1,15 @@
+#Last Character
+
+#Write a program which prints the last character of a given word.
+
+# Explanation
+
+# For example, if the given input word is "January", your code should print the last character "y".
+
+A = str(input())
+length = len(A)
+Last_word = A[length - 1]
+print(Last_word) 
+
+
+
