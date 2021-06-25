0da24c504cdd8437bd3fa700b0250b426943a959
Ushravan <39063876+Ushravan@users.noreply.github.com>
2021-06-25 14:47:00 +0530

    Create Reverse_the_digits.py

---
+++ Conditional Statements/Reverse_the_digits.py
@@ -0,0 +1,12 @@
+#Reverse the digits
+#Write a program to reverse the digits of a given two-digit number.
+
+#Explanation
+
+#For example, if the given two-digit number is 21, the reverse of 21 is 12.
+
+
+A = int(input())
+K = str(A)
+C = K[1]+K[0]
+print(C)
