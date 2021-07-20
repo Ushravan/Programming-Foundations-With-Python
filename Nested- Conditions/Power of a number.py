fb2e27216f33a4e40859704872f2087def1b3b3b
Ushravan <39063876+Ushravan@users.noreply.github.com>
2021-07-20 21:44:25 +0530

    Create Power of a number.py

---
+++ Nested- Conditions/Power of a number.py
@@ -0,0 +1,19 @@
+#Power of a number
+#Given two positive integers, which is a number and a power. Write a program to calculate the power of the given number.
+#Input
+
+#The first line of input will contain an integer, (A).
+#The second line of input will contain an integer, (B).
+#Output
+
+#The output should be a single line containing the power of the given number. (AB)
+#Explanation
+
+#For example, if the given number is 3 and the power is 2, your code should print the power of the given number (32), which is 9.
+
+A = int(input())
+B = int(input())
+result = A ** B
+print(result)
+
+
