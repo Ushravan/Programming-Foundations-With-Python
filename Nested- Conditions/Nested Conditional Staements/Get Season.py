f8a842a9ff01168e6324db0d9099e52298c1937d
Ushravan <39063876+Ushravan@users.noreply.github.com>
2021-07-21 22:26:53 +0530

    Update Get Season.py

--- Nested- Conditions/Nested Conditional Staements/Get Season.py
+++ Nested- Conditions/Nested Conditional Staements/Get Season.py
@@ -1,20 +1,20 @@
-Get Season
-Write a program to find season for the given month number.
-Input
+#Get Season
+#Write a program to find season for the given month number.
+#Input
 
-The first line of input will contain an integer that indicates the number of the month.
-If the given number of the month is 3, that indicates March month.
-Output
+#The first line of input will contain an integer that indicates the number of the month.
+#If the given number of the month is 3, that indicates March month.
+#Output
 
-If the given month is either November or December or January, print "Winter".
-If the given month is either February or March, print "Spring".
-If the given month is either April or May or June print "Summer".
-If the given month is either July or August, print "Rainy".
-If the given month is either September or October, print "Autumn".
-Explanation
+#If the given month is either November or December or January, print "Winter".
+#If the given month is either February or March, print "Spring".
+#If the given month is either April or May or June print "Summer".
+#If the given month is either July or August, print "Rainy".
+#If the given month is either September or October, print "Autumn".
+#Explanation
 
-For example, if the given month is 1, which indicates January month, the output should be "Winter".
-Similarly, if the given month is 4, which indicates April month, the output should be "Summer".
+#For example, if the given month is 1, which indicates January month, the output should be "Winter".
+#Similarly, if the given month is 4, which indicates April month, the output should be "Summer".
 
 Month = int(input())
 if (Month==12)or(Month==11)or(Month==1):
