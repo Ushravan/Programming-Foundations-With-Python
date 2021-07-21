462c9c6b452e3589a4f5a0d9c6b5b9ee3a29709a
Ushravan <39063876+Ushravan@users.noreply.github.com>
2021-07-21 22:27:57 +0530

    Update Name of the polygon.py

--- Nested- Conditions/Nested Conditional Staements/Name of the polygon.py
+++ Nested- Conditions/Nested Conditional Staements/Name of the polygon.py
@@ -1,19 +1,19 @@
-Name of the polygon
-Given the number of sides, write a program to print the name of the polygon.
-Input
+#Name of the polygon
+#Given the number of sides, write a program to print the name of the polygon.
+#Input
 
-The first line of input will contain an integer.
-Output
+#The first line of input will contain an integer.
+#Output
 
-If the given number of sides is less than 3, print "Not Polygon".
-If the given number of sides is equal to 3, print "Triangle".
-If the given number of sides is equal to 4, print "Quadrilateral".
-If the given number of sides is equal to 5, print "Pentagon".
-If the given number of sides is greater than 5, print "Big Polygon".
-Explanation
+#If the given number of sides is less than 3, print "Not Polygon".
+#If the given number of sides is equal to 3, print "Triangle".
+#If the given number of sides is equal to 4, print "Quadrilateral".
+#If the given number of sides is equal to 5, print "Pentagon".
+#If the given number of sides is greater than 5, print "Big Polygon".
+#Explanation
 
-For example, if the given number of sides is 4, the output should be "Quadrilateral".
-Similarly, if the given number of sides is 2, the output should be "Not Polygon".
+#For example, if the given number of sides is 4, the output should be "Quadrilateral".
+#Similarly, if the given number of sides is 2, the output should be "Not Polygon".
 
 
 A = int(input())
