8239097dff85f92762541dd9cff17485522f4a4b
Ushravan <39063876+Ushravan@users.noreply.github.com>
2021-07-21 22:25:34 +0530

    Update Electricity Bill.py

--- Nested- Conditions/Nested Conditional Staements/Electricity Bill.py
+++ Nested- Conditions/Nested Conditional Staements/Electricity Bill.py
@@ -1,48 +1,48 @@
-Electricity Bill
-In this problem, you need to write a program to calculate the electricity bill for a household, based on the units of electricity the household consumed. The price for unit varies based on the slab. The charges per unit for different slabs are as mentioned below:
-For the first 50 units (0-50), the charge is 2/unit
-For the next 100 units (51-150), the charge is 3/unit
-For the next 100 units (151-250), the charge is 5/unit
-For above 250 units (251 and above), the charge is 8/unit
-Apart from these charges, there is also an additional surcharge of 20% on the total amount is added to the bill.
-Input
+#Electricity Bill
+#In this problem, you need to write a program to calculate the electricity bill for a household, based on the units of electricity the household consumed. The price for unit varies based on the slab. The charges per unit for different slabs are as mentioned below:
+#For the first 50 units (0-50), the charge is 2/unit
+#For the next 100 units (51-150), the charge is 3/unit
+#For the next 100 units (151-250), the charge is 5/unit
+#For above 250 units (251 and above), the charge is 8/unit
+#Apart from these charges, there is also an additional surcharge of 20% on the total amount is added to the bill.
+#Input
 
-The input will be a single line containing an integer. 
-Output
+#The input will be a single line containing an integer. 
+#Output
 
-The output should be a single line containing the calculated bill amount. 
-Explanation
+#The output should be a single line containing the calculated bill amount. 
+#Explanation
 
-For example, if the given number of units is 50.
+#For example, if the given number of units is 50.
 
-Charges 2/unit for 0 to 50 units   =>  50 x 2   = 100
-Charges 3/unit for 51 to 150 units =>   0 x 3   =   0
-Charges 5/unit for 151 to 250      =>   0 x 5   =   0
-Charges 8/unit for 251 and above   =>   0 x 8   =   0
+#Charges 2/unit for 0 to 50 units   =>  50 x 2   = 100
+#Charges 3/unit for 51 to 150 units =>   0 x 3   =   0
+#Charges 5/unit for 151 to 250      =>   0 x 5   =   0
+#Charges 8/unit for 251 and above   =>   0 x 8   =   0
 -----------------------------------------------------
-Total                              =>             100 
-Surcharge (20 % of Bill)           => 100 x 0.2 =  20
+#Total                              =>             100 
+#Surcharge (20 % of Bill)           => 100 x 0.2 =  20
 -----------------------------------------------------
-Bill                               =>             120
------------------------------------------------------
-
-So the total bill should be 120.0
-
-
-For example, if the given number of units is 200.
-
-Charges 2/unit for 0 to 50 units   =>  50 x 2   = 100
-Charges 3/unit for 51 to 150 units => 100 x 3   = 300
-Charges 5/unit for 151 to 250      =>  50 x 5   = 250 
-Charges 8/unit for 251 and above   =>   0 x 8   =   0
------------------------------------------------------
-Total                              =>             650 
-Surcharge (20 % of Bill)           => 650 x 0.2 = 130
------------------------------------------------------
-Bill                               =>             780
+#Bill                               =>             120
 -----------------------------------------------------
 
-So the total bill should be 780.0
+#So the total bill should be 120.0
+
+
+#For example, if the given number of units is 200.
+
+#Charges 2/unit for 0 to 50 units   =>  50 x 2   = 100
+#Charges 3/unit for 51 to 150 units => 100 x 3   = 300
+#Charges 5/unit for 151 to 250      =>  50 x 5   = 250 
+#Charges 8/unit for 251 and above   =>   0 x 8   =   0
+-----------------------------------------------------
+#Total                              =>             650 
+#Surcharge (20 % of Bill)           => 650 x 0.2 = 130
+-----------------------------------------------------
+#Bill                               =>             780
+-----------------------------------------------------
+
+#So the total bill should be 780.0
 
 
 
