78a1521343ecf7ad18151fa705e4a06346582292
Ushravan <39063876+Ushravan@users.noreply.github.com>
2021-07-21 22:26:19 +0530

    Update Get Grades.py

--- Nested- Conditions/Nested Conditional Staements/Get Grades.py
+++ Nested- Conditions/Nested Conditional Staements/Get Grades.py
@@ -1,18 +1,18 @@
-Get Grades
-Write a program to calculate the grade of the student based on marks he/she scored.
-Input
+#Get Grades
+#Write a program to calculate the grade of the student based on marks he/she scored.
+#Input
 
-The first line of input will contain a float.
-Output
+#The first line of input will contain a float.
+#Output
 
-If the given marks are greater than 85, print "A".
-If the given marks are greater than 70 and less than or equal to 85, print "B".
-If the given marks are greater than or equal to 60 and less than or equal to 70, print "C".
-If the given marks are less than 60, print "F".
-Explanation
+#If the given marks are greater than 85, print "A".
+#If the given marks are greater than 70 and less than or equal to 85, print "B".
+#If the given marks are greater than or equal to 60 and less than or equal to 70, print "C".
+#If the given marks are less than 60, print "F".
+#Explanation
 
-For example, if the given marks of a student are 70.5, your code should print the grade "B".
-Similarly, if the marks of a student are 90, your code should print the grade "A".
+#For example, if the given marks of a student are 70.5, your code should print the grade "B".
+#Similarly, if the marks of a student are 90, your code should print the grade "A".
 
 Marks = float(input())
 
